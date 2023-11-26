4a902e1e57a38ec7feceecb7bba309c61606b06d
Is-monp <isabellapalencia@uninorte.edu.co>
2023-11-26 11:43:23 -0500

    main screen
    -Logo & background reworked
    -buttons added.
    -funtional buttons

---
+++ El_Legado_De_Francisco/Assets/Scripts/Script_Main menu/panel_manager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class panel_manager : MonoBehaviour
+{
+    
+    public void play_button()
+    {
+        SceneManager.LoadScene("Escena #2");
+    }
+    public void quit_button() //Esto solo va a funcionar cuando descarguemos el juego, entonces mejor que tire algo por consola a ver si si funciona lol
+    {
+        Debug.Log("chao bts");
+        Application.Quit();
+    }
+
+}
