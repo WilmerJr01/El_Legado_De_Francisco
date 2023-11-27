2ed784f78f64543e9a1d5a6de5195222f631a190
WilmerJr01 <jwsantiago@uninorte.edu.co>
2023-11-27 18:28:18 -0500

    EscenaaScore

---
+++ El_Legado_De_Francisco/Assets/GestorMarcadores.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
+
+[System.Serializable]
+public class Row
+{
+    public string nombre;
+    public int puntuacion;
+}
+
+[System.Serializable]
+public class ContenedorFilas
+{
+    public Row[] filas;
+}
+
+public class GestorMarcadores : MonoBehaviour
+{
+    public string rutaArchivoJSON;
+     
+    public  TextMeshProUGUI marcadoresTextNombre; // Asigna un objeto Text en el Inspector.
+    public  TextMeshProUGUI marcadoresTextPuntos; // Asigna un objeto Text en el Inspector.
+
+    void Start()
+    {
+        try
+        {
+            string contenidoJSON = System.IO.File.ReadAllText(rutaArchivoJSON);
+
+            // Deserializa el JSON usando JsonUtility
+            ContenedorFilas contenedor = new ContenedorFilas();
+            contenedor = JsonUtility.FromJson<ContenedorFilas>(contenidoJSON);
+
+            if (contenedor != null && contenedor.filas != null)
+            {
+                // Ordena las filas por puntaje de mayor a menor
+                contenedor.filas = contenedor.filas.OrderByDescending(f => f.puntuacion).ToArray();
+
+                // Ahora puedes mostrar los marcadores en un objeto de texto
+                foreach (var fila in contenedor.filas)
+                {
+                    marcadoresTextNombre.text += $"{fila.nombre}\n";
+                    marcadoresTextPuntos.text += $"{fila.puntuacion}\n";
+
+                }
+            }
+            else
+            {
+                Debug.LogError("ContenedorFilas o filas es nulo.");
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"Error al deserializar el JSON: {e.Message}");
+        }
+    }
+}
--- El_Legado_De_Francisco/Assets/Scripts/Script_Main menu/panel_manager.cs
+++ El_Legado_De_Francisco/Assets/Scripts/Script_Main menu/panel_manager.cs
@@ -8,20 +8,19 @@ public class panel_manager : MonoBehaviour
     
     public void play_button()
     {
-        SceneManager.LoadScene("Escena #1");
+        SceneManager.LoadScene(1);
     }
     public void quit_button() //Esto solo va a funcionar cuando descarguemos el juego, entonces mejor que tire algo por consola a ver si si funciona lol
     {
-        Debug.Log("chao bts");
         Application.Quit();
     }
     public void scores_button() //Esto solo va a funcionar cuando descarguemos el juego, entonces mejor que tire algo por consola a ver si si funciona lol
     {
-        SceneManager.LoadScene("Scores");
+        SceneManager.LoadScene(3);
     }
     public void back_button()
     {
-        SceneManager.LoadScene("Main menú");
+        SceneManager.LoadScene(0);
     }
 
 }
