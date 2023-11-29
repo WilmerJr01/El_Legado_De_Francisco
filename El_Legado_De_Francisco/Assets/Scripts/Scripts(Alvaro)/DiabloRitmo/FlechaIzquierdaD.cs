c62bf62260db79b8a056444b6d33785fe15d3a80
doriada <doriada@uninorte.edu.co>
2023-11-28 21:30:20 -0500

    nivel de ritmo diablo

---
+++ El_Legado_De_Francisco/Assets/Scripts/Scripts(Alvaro)/DiabloRitmo/FlechaIzquierdaD.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlechaIzquierdaD : MonoBehaviour
+{
+    public float velocidad;
+    public int contador = 0;
+    public bool adentro = false;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * -velocidad * Time.deltaTime;
+        if (contador == 2)
+        {
+            adentro = true;
+        }
+        else
+        {
+            adentro = false;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (adentro == true)
+            {
+                GameObject.Find("CasillaJugadorD").GetComponent<JugadorDiablo>().puntaje++;
+                GameObject.Find("CasillaJugadorD").GetComponent<JugadorDiablo>().texto.text = "Puntaje: " +
+                    GameObject.Find("CasillaJugadorD").GetComponent<JugadorDiablo>().puntaje.ToString();
+                Destroy(gameObject);
+            }
+        }
+
+
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            contador++;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            contador--;
+        }
+    }
+}
---
+++ El_Legado_De_Francisco/Assets/Scripts/Scripts(Alvaro)/DiabloRitmo/JugadorDiablo.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JugadorDiablo : MonoBehaviour
+{
+    public int puntaje = 0;
+    public TextMeshProUGUI texto;
+    public bool adentro = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        texto = GameObject.Find("ScoreUI").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (puntaje == 5)
+        {
+            SceneManager.LoadScene(4);
+        }
+    }
+}
